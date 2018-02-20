7cabfe428d63c105fd10d3db4639a25650096678
Ivan Nosov <ivan7en@gmail.com>
2018-02-20 16:31:24 +0200

    add dependent destroy

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,5 +4,5 @@ describe Question do
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-  it { should have_many :answers }
+  it { should have_many(:answers).dependent(:destroy) }
 end
