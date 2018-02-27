69628e8808d11e0943bb0ad878b3816fd8d9de3a
Ivan Nosov <ivan7en@gmail.com>
2018-02-27 17:46:32 +0200

    adding file to question

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe Answer do
-  it { should validate_presence_of :body }
   it { should belong_to :question }
   it { should belong_to(:user) }
+  it { should validate_presence_of :body }
   it { should validate_uniqueness_of(:best).scoped_to(:question_id) }
 end
