c90d0522ded2782cf00105535760d03273db86bb
Ivan Nosov <ivan7en@gmail.com>
2018-03-22 19:30:12 +0200

    add schema load task

---
+++ config/deploy.rb
@@ -0,0 +1,41 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.1"
+
+set :application, "qa"
+set :repo_url, "git@github.com:IvanNosov/qa.git"
+set :branch, 'lesson_20'
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/developer/qa"
+set :deploy_user, 'developer'
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml",".env"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+namespace :deploy do
+  desc 'Restart application' do 
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+        execute :touch, release_path.join('tmp/restart.txt')
+      end 
+    end
+  end
+  namespace :db do
+    desc "Load the database schema if needed"
+    task load: [:set_rails_env] do
+      on primary :db do
+        if not test(%Q[[ -e "#{shared_path.join(".schema_loaded")}" ]])
+          within release_path do
+            with rails_env: fetch(:rails_env) do
+              execute :rake, "db:schema:load"
+              execute :touch, shared_path.join(".schema_loaded")
+            end
+          end
+        end
+      end
+    end
+  end
+  before "deploy:migrate", "deploy:db:load"
+  after :publishing, :restart
+end
